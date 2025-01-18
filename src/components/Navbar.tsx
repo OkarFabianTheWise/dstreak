72fc1112d9b0d6d48a2412e54b311b41335138c8
geek-abyte <osisegabriel@gmail.com>
2025-01-18 13:55:25 +0100

    rebase

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,12 +8,10 @@ const Navbar = () => {
     return (
         <nav className="px-4 flex justify-between items-center sticky z-50 top-0 p-4 bg-secondary"> 
             <img src={logo} alt="devstreak"  className="h-8 bg-transparent"/>
-            <div className='menu text-primary'>
-                <ul>
+                <ul className='menu font-semibold text-primary flex gap-4'>
                     <li className='text-primary'>Home</li>
                     <li className='text-primary'>Community</li>
                 </ul>
-            </div>
             <button 
                 onClick={() => navigate('/login')}
                 className="rounded-md px-4 py-2 bg-primary uppercase text-secondary font-bold hover:bg-secondary hover:border hover:text-primary transition-all duration-300"
