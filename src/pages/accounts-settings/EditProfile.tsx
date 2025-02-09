0ae251cf36ba53f2260f53af004f8a3e48d641e1
Anthony Ameh <antonineutron@gmail.com>
2025-02-09 19:53:59 +0100

    worked on linkedAccounts - users can set their social handles, EditProfile - user email and name now showing

--- src/pages/accounts-settings/EditProfile.tsx
+++ src/pages/accounts-settings/EditProfile.tsx
@@ -7,6 +7,7 @@ import { handleProfileUpdate, handleDeleteAccount, useUserStore } from "../../ut
 import AlertModal from "@/components/ui/api-error-alert";
 import ApiCallConfirm from "@/components/ui/api-call-confirmation";
 import ApiSuccessAlert from "@/components/ui/api-success-alert";
+import { demoProfilePics } from "@/assets/image";
 
 
 const EditProfile = () => {
@@ -75,7 +76,7 @@ const EditProfile = () => {
             <div>
               <h2 className="text-foreground mb-3 sm:mb-4">Avatar</h2>
               <div className="flex flex-col items-start gap-3 sm:gap-4">
-                <div className="w-[80px] h-[80px] sm:w-[100px] sm:h-[100px] rounded-xl border border-primary flex items-center justify-center">
+                <div className="w-[80px] h-[80px] sm:w-[100px] sm:h-[100px] rounded-xl hover:opacity-50 flex items-center justify-center">
                   {avatar ? (
                     <img
                       src={avatar}
@@ -83,14 +84,14 @@ const EditProfile = () => {
                       className="w-full h-full object-cover"
                     />
                   ) : (
-                    <span className="text-muted-foreground text-xs sm:text-sm pl-2">
-                      No image
-                    </span>
-                      // <img
-                      //   src={demoProfilePics}
-                      //   alt="Avatar"
-                      //   className="w-full h-full object-cover"
-                      // />
+                    // <span className="text-muted-foreground text-xs sm:text-sm pl-2">
+                    //   No image
+                    // </span>
+                      <img
+                        src={demoProfilePics}
+                        alt="Avatar"
+                        className="w-full h-full p-2 object-cover"
+                      />
                   )}
                 </div>
                 <div>
